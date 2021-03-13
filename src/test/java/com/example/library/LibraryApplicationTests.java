8fefcafb078deafda93349872bd141b99a741206
jefferson <mfljefferson@gmail.com>
2021-03-13 20:45:47 -0300

    initial commit

---
+++ src/test/java/com/example/library/LibraryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.library;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	@Ignore
+	void contextLoads() {
+	}
+
+}
