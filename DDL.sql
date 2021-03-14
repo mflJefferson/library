3193757366b8ba31030f59e5ebc8f045a43ba6f9
jefferson <mfljefferson@gmail.com>
2021-03-13 21:03:20 -0300

    fix configs

---
+++ DDL.sql
@@ -0,0 +1,24 @@
+create schema library;
+
+use library;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, insert, delete, update on library.* to user@'localhost';
+
+create table books
+(
+    id int not null,
+    title varchar(255) not null,
+    isbn varchar(64) not null,
+    description text not null,
+    created_at datetime not null,
+    updated_at datetime null
+);
+
+create unique index books_id_uindex
+	on books (id);
+
+alter table books
+    add constraint books_pk
+        primary key (id);
